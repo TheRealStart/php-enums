b833396dd0aea480e359a9843eba7e66976e8d50
grr89 <grr89@mail.ru>
2016-05-13 17:41:26 +0500

    Initial commit

---
+++ BaseEnum.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @author: Rustam Akhmedov
+ * Date: 2/5/13
+ * Time: 3:57 PM
+ */
+
+namespace common\enums;
+
+class BaseEnum
+{
+    protected static $_enums = array();
+
+    public static function getClass()
+    {
+        return get_called_class();
+    }
+
+    /**
+     * Returns array of $constName => $constValue
+     * @static
+     * @return array
+     */
+    public static function getEnums()
+    {
+        $className = get_called_class();
+        if (!isset( self::$_enums[$className] )) {
+            $class = new \ReflectionClass($className);
+            return self::$_enums[$className] = $class->getConstants();
+        } else
+            return self::$_enums[$className];
+    }
+
+    public static function getEnum($value)
+    {
+        $data = self::getEnums();
+        $key  = mb_strtoupper($value);
+
+        if (isset( $data[$key] ))
+            return $data[$key];
+
+        return null;
+    }
+
+    public static function getUiEnums()
+    {
+        $data = array_flip(self::getEnums());
+        foreach ($data as &$value)
+            $value = static::normalizeName($value);
+        return $data;
+    }
+
+    public static function getLabel($value)
+    {
+        $labels = array_flip(self::getEnums());
+        if (isset( $labels[$value] )) {
+            return get_called_class() . '.' . $labels[$value];
+        } else {
+            return null;
+        }
+
+    }
+
+    public static function getUiLabel($value)
+    {
+        if ($value === null)
+            return null;
+
+        $labels = array_flip(self::getEnums());
+
+        if (isset( $labels[$value] ))
+            return static::normalizeName($labels[$value]);
+        else
+            return null;
+    }
+
+    /**
+     * User Friendly - $constValue => Translated $constKey
+     * @static
+     * @param string $scope - Language file
+     * @return array
+     */
+    public static function getEnumsView($scope = 'app')
+    {
+        $class = get_called_class();
+        return array_map(
+            function ($string) use ($scope, $class) {
+                return \Yii::t($scope, $class . '.' . $string);
+            },
+            array_flip(self::getEnums())
+        );
+    }
+
+    protected static function normalizeName($value)
+    {
+        return ucwords(strtolower(str_replace("_", " ", $value)));
+    }
+}
